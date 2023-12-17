a32da0eedb0a058dc608d77de1dd07d695240f25
Khyati Thakur <khyati@inspiritvr.com>
2023-12-17 13:56:58 +0530

    update new backend endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 // export const API_URL: string = 'http://localhost:3001'
-export const API_URL: string = 'https://smashbotbackend.azurewebsites.net'
+export const API_URL: string = 'https://smashbotbackendnew.azurewebsites.net';
 
