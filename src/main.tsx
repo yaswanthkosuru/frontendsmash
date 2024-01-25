48a1567750b31833d2c5a7fc11289f6c34329c61
Khyati Thakur <khyati@providentia.io>
2024-01-25 05:26:53 +0000

    Fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import Interview from './pages/Interview.tsx'
+import ReactDOM from 'react-dom/client'
 import { v4 as uuidv4 } from 'uuid'
+import Interview from './pages/Interview.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<ChakraProvider>
-		<Interview name="test" smashUserId={uuidv4()} botPreference="male" />
+		{/* <Interview name="test-with-fixes" smashUserId={'0fb20906-cb60-440b-8bce-03019cacc7f1'} /> */}
+		<Interview name="test-with-fixes" smashUserId={uuidv4()} />
 	</ChakraProvider>,
 )
