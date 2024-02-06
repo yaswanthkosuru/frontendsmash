15c0aa67e3abd342ec00e060afeacaae5cafc669
Khyati Thakur <khyati@inspiritvr.com>
2024-02-06 17:29:03 +0530

    update img

--- src/components/InterviewCompletedScreen.tsx
+++ src/components/InterviewCompletedScreen.tsx
@@ -1,13 +1,13 @@
 import { Image, VStack } from '@chakra-ui/react'
 
 const InterviewCompletedScreen = () => {
-	//const imgUrl = new URL('./images/interview_end.png', import.meta.url).href
+	const imgUrl = new URL('/images/interview_end.png', import.meta.url).href
 
 	return (
 		<>
 			<VStack spacing={2} p={4} justifyContent={'center'} alignItems={'center'} h={'100vh'}>
 				<Image
-					src={'/interview_end.png'}
+					src={imgUrl}
 					h={'90vh'}
 					style={{
 						borderRadius: '30px',
