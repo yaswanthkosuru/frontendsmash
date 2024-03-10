57cae8e27b188f07791e0aedecf058a63d3b30b7
unknown <yaswanthkosuru999@gmail.com>
2024-03-10 15:29:14 +0530

    prod

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-// export const API_URL: string = 'http://localhost:3001'
-export const API_URL: string = 'https://smashbotbackendnew.azurewebsites.net'
+export const API_URL: string = 'http://localhost:3001'
+//export const API_URL: string = 'https://smashbotbackendnew.azurewebsites.net'
